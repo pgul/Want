8c5514860e9fe48b0c7113c0b3ca3f2437d64911
Pavel Gulchuk <gul@ytechteam.com>
2022-01-01 14:50:21 +0200

    Set version 0.29.1 (to be more than ubuntu-packaged 0.29-1build4)

--- Want.pm
+++ Want.pm
@@ -12,7 +12,7 @@ our @ISA = qw(Exporter DynaLoader);
 
 our @EXPORT = qw(want rreturn lnoreturn);
 our @EXPORT_OK = qw(howmany wantref);
-our $VERSION = '0.28';
+our $VERSION = '0.29.1';
 
 bootstrap Want $VERSION;
 
